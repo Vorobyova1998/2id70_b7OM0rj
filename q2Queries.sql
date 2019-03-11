a4f721726a12a7176d69997b9d705e694ee0eea7
Alina <alinavorobyova8@gmail.com>
2019-03-11 11:11:54 +0100

    Update q2Queries.sql

--- q2Queries.sql
+++ q2Queries.sql
@@ -1,4 +1,4 @@
-SELECT 0;
+SELECT	Courses.CourseName, CourseRegistrations.Grade ORDER BY CourseOffers.year, CourseOffers.Quartile FROM 	StudentRegistrationToDegrees, CourseRegistrations, CourseOffers, Courses WHERE	StudentRegistrationToDegrees.StudentId = %1% AND StudentRegistrationToDegrees.DegreeId = %2% AND StudentRegistrationToDegrees.StudentRegistrationId = CourseRegistrations.StudentRegistrationId AND CourseRegistrations.CourseOfferId = CourseOffers.CourseOfferId AND 	StudentRegistrationToDegrees.DegreeId = Courses.DegreeId AND CourseOffers.CourseId = Courses.CourseId;												
 SELECT 0;
 SELECT 0;
 SELECT 0;
